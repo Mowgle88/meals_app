4c67eec78f8b925021c19b2c92500d1b1a95d252
Dmitry Ilyin <mowgle>
2023-05-26 00:02:12 +0300

    feat: add meal deetailss screen

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -5,8 +5,13 @@ import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
   final Meal meal;
+  final void Function(Meal meal) onSelectMeal;
 
-  const MealItem({super.key, required this.meal});
+  const MealItem({
+    super.key,
+    required this.meal,
+    required this.onSelectMeal,
+  });
 
   String get comlexityText {
     final complexity = meal.complexity.name;
@@ -28,7 +33,9 @@ class MealItem extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          onSelectMeal(meal);
+        },
         child: Stack(
           children: [
             FadeInImage(
